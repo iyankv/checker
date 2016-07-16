aa14d6468b61e16fa749ee3191b5fc6162b095da
iyan.kv <spencer.saril.7@gmail.com>
2016-07-16 17:38:32 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,6 +1,12 @@
-apt-get install libapache2-mod-python python-geoip python-ipaddr python-humanize apache2 git
+apt-get -y install libapache2-mod-python python-geoip python-ipaddr python-humanize apache2 git
 cd /var/www/html
-wget
+wget https://raw.githubusercontent.com/iyankv/checker/master/conf/check.tar
+tar -xvf check.tar
+rm check.tar
+cd /etc/apache2/sites-enabled/
+rm 000-default.conf
+wget https://raw.githubusercontent.com/iyankv/checker/master/conf/000-default.conf
+/etc/init.d/apache2 restart
 a2enmod python cgid
 cd /usr/share/GeoIP/
 wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
